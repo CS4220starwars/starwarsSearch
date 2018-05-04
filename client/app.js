3d514ce3f7bd08775d62c7728ee1699b82879e9d
briank <brian.a.kan@gmail.com>
2018-05-03 22:02:18 -0700

    Clicking on Details works

--- client/app.js
+++ client/app.js
@@ -1,3 +1,16 @@
+const searchComponent = {
+    template: ` <div @click="clickEvent">
+                     <span v-html="content.name"></span>
+                     <br/>
+                </div>`,
+    props: ['content'],
+    methods: {
+        clickEvent() {
+            this.$emit('click')
+        }
+    }
+}
+
 let starwars = new Vue({
     el: '#starwars',
     data: {
@@ -6,22 +19,44 @@ let starwars = new Vue({
         selectedCategory: '', // Category selected in the <select> tag
         searchQuery: '', // Search query the user inputs
         categories: [],
-        searchResults: []
+        searchResults: [],
+        searchResultsArray: '',
+        isViewingDetails: false,
+        currentDetails: ''
+
     },
     methods: {
         searchApi: function () {
             axios.get(`http://localhost:8080/search?category=${this.selectedCategory}&query=${this.searchQuery}`)
                 .then(response => {
+                    let index = 1
                     response.data.results.forEach((result) => {
-                        if (result.name != null)
-                            this.searchResults.push(result.name)
-                        else
-                            this.searchResults.push(result.title)
+                        const newResultObj = {}
+                        if (result.name != null) {
+                            newResultObj.name = result.name
+                            newResultObj.index = index
+                            this.searchResults.push(newResultObj)
+                        }
+                        else { // To handle film titles instead of character names
+                            newResultObj.name = result.title
+                            newResultObj.index = index
+                            this.searchResults.push(newResultObj)
+                        }
+                        index++;
                     })
+                    this.searchResultsArray = response.data.results
                 })
+        },
+        viewDetails: function (number) {
+            this.currentDetails= this.searchResultsArray[number-1]
+            this.isViewingDetails=true;
         },
         clear: function () {
             this.searchResults = []
+        },
+        reset: function (){
+            this.isViewingDetails=false;
+            this.searchResults= []
         }
     },
     mounted() {
@@ -36,6 +71,8 @@ let starwars = new Vue({
         }).catch((err) => {
             alert(err)
         })
-
+    },
+    components: {
+        'search-component': searchComponent
     }
 })
